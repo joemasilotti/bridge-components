b79cac19278a75bc09f750e4e985647f041439fc
Joe Masilotti <joe@masilotti.com>
2025-03-13 12:50:55 -0700

    Add missing import statement

--- components/menu/menu_controller.js
+++ components/menu/menu_controller.js
@@ -1,4 +1,5 @@
 import { BridgeComponent } from "@hotwired/hotwire-native-bridge"
+import { BridgeElement } from "@hotwired/hotwire-native-bridge"
 
 export default class extends BridgeComponent {
   static component = "menu"

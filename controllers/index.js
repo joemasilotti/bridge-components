8f4d90e6d95be0d116e495caeb8a859609264f42
Joe Masilotti <joe@masilotti.com>
2025-09-06 14:54:34 -0700

    Add missing Stimulus controller reference

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ import ButtonBridgeController from "../components/button/button_controller.js"
 import FormBridgeController from "../components/form/form_controller.js"
 import HapticBridgeController from "../components/haptic/haptic_controller.js"
 import MenuBridgeController from "../components/menu/menu_controller.js"
+import ReviewPromptBridgeController from "../components/review-prompt/review_prompt_controller.js"
 import SearchBridgeController from "../components/search/search_controller.js"
 import ShareBridgeController from "../components/share/share_controller.js"
 import ThemeBridgeController from "../components/theme/theme_controller.js"
@@ -14,6 +15,7 @@ export {
   FormBridgeController,
   HapticBridgeController,
   MenuBridgeController,
+  ReviewPromptBridgeController,
   SearchBridgeController,
   ShareBridgeController,
   ThemeBridgeController,
@@ -27,6 +29,7 @@ export const controllers = [
   { identifier: "bridge--form",   controllerConstructor: FormBridgeController },
   { identifier: "bridge--haptic", controllerConstructor: HapticBridgeController },
   { identifier: "bridge--menu",   controllerConstructor: MenuBridgeController },
+  { identifier: "bridge--review-prompt", controllerConstructor: ReviewPromptBridgeController },
   { identifier: "bridge--search", controllerConstructor: SearchBridgeController },
   { identifier: "bridge--share",  controllerConstructor: ShareBridgeController },
   { identifier: "bridge--theme",  controllerConstructor: ThemeBridgeController },
